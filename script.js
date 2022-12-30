f1577d400a56818864e9f9cd5c623f04efd61d9f
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2022-12-30 23:20:13 +0000

    exerciece Ninja

---
+++ script.js
@@ -0,0 +1,37 @@
+// Exercice 1 : Évaluation
++
+5 >= 1
+// true
+0 === 1
+// false
+    4 <= 1
+//   false
+    1 != 1
+// false
+    "A" > "B"
+// true
+
+    "B" < "C"
+// true
+    "a" > "A"
+ // true
+    "b" < "A"
+// false
+    true === false
+// false
+    true != true
+// false
+
+    // Exercice 2
+
+    //   let nombre = prompt("rentrer les nombres à virgules");
+    //   let tab = nombre.split(",");
+    //   let sum =(x,y)  => parseInt(x) + parseInt(y);
+    //   console.log(tab.reduce(sum));   
+
+    //   Exercice 3 : Trouver Nemo
+    let str = prompt(" Entrer une phrase contenat Nemo");
+    let chercher = str.search("Nemo");
+    console.log(`I found Nemo at chercher ${chercher}`);
+
+//exrecice 4
