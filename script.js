c1a10cfdfc8a8238c92e9cc21443ef2acd3d8b01
SAID DIARRASSOUBA <seydou.diarrassouba@imako.digital>
2023-01-10 17:10:10 +0000

    ninja

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Exercice 1 : Évaluation
-+
+
 5 >= 1
 // true
 0 === 1
@@ -24,14 +24,34 @@
 
     // Exercice 2
 
-    //   let nombre = prompt("rentrer les nombres à virgules");
-    //   let tab = nombre.split(",");
-    //   let sum =(x,y)  => parseInt(x) + parseInt(y);
-    //   console.log(tab.reduce(sum));   
+      let nombre = prompt("rentrer les nombres à virgules");
+      let tab = nombre.split(",");
+      let sum =(x,y)  => parseInt(x) + parseInt(y);
+      console.log(tab.reduce(sum));   
+   
 
-    //   Exercice 3 : Trouver Nemo
-    let str = prompt(" Entrer une phrase contenat Nemo");
-    let chercher = str.search("Nemo");
-    console.log(`I found Nemo at chercher ${chercher}`);
+   
+/*Exercise 3 : Find Nemo*/
 
-//exrecice 4
+let userSentence = prompt("Entrez une phrase contenant le mot Nemo");
+
+let position = userSentence.search("Nemo");
+
+console.log(`I found Nemo at ${position}`);
+
+/*Exercise 4 : Boom*/
+
+let num = prompt("Please enter a number:");
+const result = "boom"
+if (num < 2) {
+  console.log("boom");
+} else if (num % 2 === 0 && num % 5 === 0) {
+  console.log("BOOM!".toUpperCase());
+} else if (num % 2 === 0) {
+  console.log("boom!".toLowerCase());
+} else if (num % 5 === 0) {
+  console.log("BOOM");
+} 
+if(num> 2){
+  console.log("B" + "o".repeat(num) + "m");
+}
